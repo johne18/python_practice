3126fa5e050dcd69753609fd101e915c413e300f
John Santiago <johne.santiago@gmail.com>
2026-08-23 18:48:38 -0400

    setting a rate_limiter

--- project_one_dir/async/project_async.py
+++ project_one_dir/async/project_async.py
@@ -3,38 +3,51 @@ import aiohttp
 import random
 import time
 
+from aiolimiter import AsyncLimiter
 from project_one_dir.constants import POKEMON_IDS, POKEMON_API
 
 
-async def fetch_pokemon(session, semaphore, pokemon_id):
+# def aggregate_pokemon_data_by_type
+
+
+async def fetch_pokemon(session, semaphore, rate_limiter, pokemon_id):
     pokemon_api_id = POKEMON_API + f"/pokemon/{pokemon_id}"
 
     async with semaphore:
-        try:
-            async with session.get(pokemon_api_id, timeout=5) as response:
-                data = await response.json()
-                # print(f"Fetched data for Pokemon ID {pokemon_id}: {data['name']}")
-                return data
-        except Exception as e:
-            print(f"Error fetching data for Pokemon ID {pokemon_id}: {e}")
-            return None
+        async with rate_limiter:
+            try:
+                async with session.get(pokemon_api_id, timeout=5) as response:
+                    data = await response.json()
+                    # print(f"Fetched data for Pokemon ID {pokemon_id}: {data['name']}")
+                    return data
+            except Exception as e:
+                print(f"Error fetching data for Pokemon ID {pokemon_id}: {e}")
+                return None
 
 
 async def main():
-    num_pokemon_ids = 5_000
+    num_pokemon_ids = 250
     for _ in range(num_pokemon_ids):
         pokemon_id = random.randint(1, 1_000)
         POKEMON_IDS.append(pokemon_id)
 
     semaphore = asyncio.Semaphore(25)  # Limit the number of concurrent requests
+    rate_limiter = AsyncLimiter(100, 3) # Limit to 100 requests every n seconds
     start_time = time.time()
 
     async with aiohttp.ClientSession() as session:
-        tasks = [fetch_pokemon(session, semaphore, pokemon_id) for pokemon_id in POKEMON_IDS]
-        # pokemon_data_list = await asyncio.gather(*tasks)
-        await asyncio.gather(*tasks)
+        tasks = [fetch_pokemon(session, semaphore, rate_limiter, pokemon_id) for pokemon_id in POKEMON_IDS]
+        pokemon_data_list = await asyncio.gather(*tasks)
 
     end_time = time.time()
+
+    print(f"First 10 fetched Pokemon data:")
+    for pokemon in pokemon_data_list[:10]:
+        print("="*80)
+        print(pokemon["name"])
+        print(pokemon["types"])
+        print("="*80)
+
     print(f"Total time taken to fetch data for {num_pokemon_ids} Pokemon IDs: {end_time - start_time:.2f} seconds")
 
 if __name__ == "__main__":
